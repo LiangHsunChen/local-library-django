c26170884f1f5458e6c968d5637706df96bc29d2
LiangHsunChen <lianghsunchen@gmail.com>
2024-05-31 23:14:51 +1000

    Update list layout in admin site

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -17,6 +17,7 @@ class BookAdmin(admin.ModelAdmin):
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name',
                     'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 
 # Register the admin class with the associated model
@@ -32,6 +33,15 @@ admin.site.register(Genre)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ('display_book_instance', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 
 admin.site.register(Language)
